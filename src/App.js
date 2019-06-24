e5d9078b8c822cd45a7942f7c2d8d074770a604b
HubM <hubmoncenis@gmail.com>
2019-06-24 11:24:19 +0200

    :sparkles: adding custom patrimoine models with concepts

--- src/App.js
+++ src/App.js
@@ -42,88 +42,29 @@ export class App extends React.Component {
     return blob;
   }
 
-  // componentWillMount() {
-  //   appClarifai.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
-  //     .then(generalModel => {
-  //       return generalModel.predict("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Balaeniceps_rex_qtl1.jpg/290px-Balaeniceps_rex_qtl1.jpg");
-  //     })
-  //     .then(response => {
-  //       var concepts = response['outputs'][0]['data']['concepts']
-  //       console.log(concepts);
-  //     })
-  // }
-
   onTakePhoto (dataUri) {
     this.setState({
       pic: dataUri
     })
 
-    // console.log(dataUri)
-
-  
-
-    // const block = dataUri.split(';');
-    // const contentType = block[0].split(":")[1];
-    // const realData = block[1].split(",")[1];
-
-    // const blob = this.b64toBlob(realData, contentType);
-
-    // // fd.append('image_data', dataUri)
-    // const formDataToUpload = new FormData();
-    // formDataToUpload.append("image", blob);
-
-    // // const config = { headers: { 'Content-Type': 'multipart/form-data' } };
-    const base64String = Buffer.from(dataUri).toString("base64");
-
-    // console.log(appClarifai);
-    appClarifai.models.initModel({id: 'patrimoine', version: "421a18466de4434b99fe78704f644e7c"})
-    .then(customModel => {
-      return customModel.predict("https://live.staticflickr.com/8199/8222255700_344a8339e9_b.jpg");
-      // return customModel.predict("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Balaeniceps_rex_qtl1.jpg/290px-Balaeniceps_rex_qtl1.jpg");
+    axios.post('/api/image', {
+      picture: dataUri
     })
     .then(response => {
-      var concepts = response['outputs'][0]['data']['concepts']
-      console.log("TRAIN IA", concepts);
-      this.setState({selectedPredictions: concepts})
+      appClarifai.models.initModel({id: 'patrimoine', version: "421a18466de4434b99fe78704f644e7c"}).then(customModel => {
+        return customModel.predict("https://cdp2021.herokuapp.com/out.jpg");
+      })
+      .then(response => {
+        var concepts = response['outputs'][0]['data']['concepts']
+        console.log("TRAIN IA", concepts);
+        this.setState({selectedPredictions: concepts})
+      })
     })
-    // appClarifai.models.predict({id: 'statue', version: "00d13a21528f4ba683c4d979dbc972df"}, base64String).then(response => {
-    //   console.log(response.outputs[0].data.concepts);
-    // })
-
-    // axios.post('/api/image', {
-    //   picture: dataUri
-    // })
-    // .then(function (response) {
-    //   // console.log(response);
-    //   const { url } = response.data;
-      
-    //   // appClarifai.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
-    //   //   .then(generalModel => {
-    //   //     return generalModel.predict(url);
-    //   //   })
-    //   //   .then(response => {
-    //   //     var concepts = response['outputs'][0]['data']['concepts'];
-
-    //   //     console.log("concepts", concepts);
-
-    //   //     let selectedPredictions = concepts.filter(prediction => prediction.value >= 0.95);
-          
-    //   //     console.log("selectedPredictions", selectedPredictions);
-
-    //   //     this.setState({
-    //   //       selectedPredictions
-    //   //     })
-    //   //   })
-
-   
-      
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
+    .catch(error => {
+      console.error(error);
+    })
 
 
-    // console.log('takePhoto', dataUri);
   }
 
   onCameraError (error) {
@@ -159,8 +100,8 @@ export class App extends React.Component {
           onCameraStop = { () => { this.onCameraStop(); } }
         />
         <h1>Predictions tests</h1>
-        <div>Results > 0.95</div>
-        {
+        {/* <div>Results > 0.95</div> */}
+        {/* {
           this.state.selectedPredictions &&
           this.state.selectedPredictions.map(predict => 
             <div>
@@ -168,7 +109,9 @@ export class App extends React.Component {
               <div>{predict.value}</div>
             </div>
           )
-        }
+        } */}
+
+        <h2>Il semblerait que tu sois => {this.state.selectedPredictions[0].name}</h2>
       </div>
     );
   }
