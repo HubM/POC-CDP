56698685046ad1f69f753b633d3d0a305d79243d
HubM <hubmoncenis@gmail.com>
2019-06-24 15:20:34 +0200

    :bug: fix bug camera disabled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import axios from "axios";
-
 import Clarifai from "clarifai";
-
 import Camera, { FACING_MODES, IMAGE_TYPES }  from 'react-html5-camera-photo';
+
 import 'react-html5-camera-photo/build/css/index.css';
 
 const appClarifai = new Clarifai.App({
@@ -11,35 +10,17 @@ const appClarifai = new Clarifai.App({
 })
 
 
+// var localStream;
 
-
+// onUserMediaSuccess = function(stream) {
+//    // attach to a video element
+//    ...
+//    // keep a reference
+//    localStream = stream;
+// };
 export class App extends React.Component {
   state = {
     pic: null
-  }
-
-  b64toBlob = (b64Data, contentType, sliceSize) => {
-    contentType = contentType || '';
-    sliceSize = sliceSize || 512;
-
-    var byteCharacters = atob(b64Data);
-    var byteArrays = [];
-
-    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-        var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-        var byteNumbers = new Array(slice.length);
-        for (var i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-        }
-
-        var byteArray = new Uint8Array(byteNumbers);
-
-        byteArrays.push(byteArray);
-    }
-
-    var blob = new Blob(byteArrays, {type: contentType});
-    return blob;
   }
 
   onTakePhoto (dataUri) {
@@ -55,6 +36,10 @@ export class App extends React.Component {
         return customModel.predict("https://cdp2021.herokuapp.com/out.jpg");
       })
       .then(response => {
+        this.setState({
+          photoTaken: true
+        })
+
         var concepts = response['outputs'][0]['data']['concepts']
         console.log("TRAIN IA", concepts);
       
@@ -67,32 +52,13 @@ export class App extends React.Component {
             noPlace: true
           })
         }
-        
       })
     })
     .catch(error => {
       console.error(error);
     })
-
-
-  }
-
-  onCameraError (error) {
-    console.error('onCameraError', error);
   }
  
-  onCameraStart (stream) {
-    console.log('onCameraStart');
-  }
- 
-  onCameraStop () {
-    console.log('onCameraStop');
-    this.setState({
-      photoTaken: true
-    })
-  }
-
-
   render() {
     return (
       <div className="App">
@@ -101,7 +67,6 @@ export class App extends React.Component {
             <div>
               <Camera
                 onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
-                onCameraError = { (error) => { this.onCameraError(error); } }
                 idealFacingMode = {FACING_MODES.ENVIRONMENT}
                 idealResolution = {{width: 640, height: 480}}
                 imageType = {IMAGE_TYPES.JPG}
@@ -112,8 +77,6 @@ export class App extends React.Component {
                 isDisplayStartCameraError = {true}
                 isFullscreen = {false}
                 sizeFactor = {1}
-                onCameraStart = { (stream) => { this.onCameraStart(stream); } }
-                onCameraStop = { () => { this.onCameraStop(); } }
               />
             </div>
             : 
