1adb9d226ddae40937e094f30038ba91047f155a
HubM <hubmoncenis@gmail.com>
2019-06-25 13:37:50 +0200

    :construction: WIP on mobileOrTabletDetection

--- src/App.js
+++ src/App.js
@@ -6,15 +6,29 @@ import GMap from "./GMap";
 import CameraPhoto from "./CameraPhoto";
 import Infobulle from "./Infobulle";
 
+import { isMobileOrTablet } from "./utils";
+
 export class App extends React.Component {
   render() {
+    let initialView;
+    // if (isMobileOrTablet()) {
+      initialView = 
+        <Router>
+          <Route path='/' component={Home} />
+          <Route path="/map" component={GMap}/>
+          <Route path="/camera" component={CameraPhoto} />
+          <Route path="/infobulle" component={Infobulle} />
+        </Router>
+    // } else {
+      // initialView = 
+        // <div>
+          {/* <h1>you should use this website on your mobile</h1> */}
+        {/* </div> */}
+    // }
     return (
-      <Router>
-        <Route path='/' component={Home} />
-        <Route path="/map" component={GMap}/>
-        <Route path="/camera" component={CameraPhoto} />
-        <Route path="/infobulle" component={Infobulle} />
-      </Router>
+      <section>
+        {initialView}
+      </section>
     );
   }
 }  
