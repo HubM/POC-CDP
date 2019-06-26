9044d3429cefe26f4491a2c24cc1a13ad2615536
HubM <hubmoncenis@gmail.com>
2019-06-26 15:27:49 +0200

    :bug: bug build

--- src/Routeur.js
+++ src/Routeur.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import { HashRouter as Router, Route} from "react-router-dom";
 
-import Nav from "./Nav";
 import GMap from "./GMap";
 import CameraPhoto from "./CameraPhoto";
 import Place from "./Place";
 
 import { isMobileOrTablet } from "./utils";
 
-import { EmitProvider } from "react-emit";
 
 export class Routeur extends React.Component {
   render() {
     let initialView;
     // if (isMobileOrTablet()) {
       initialView = 
-        <EmitProvider>
-          <Router>
-            <Route exact path='/' component={GMap} />
-            <Route path="/camera" component={CameraPhoto} />
-            <Route path="/place" component={Place} />
-          </Router>
-        </EmitProvider>
+        <Router>
+          <Route exact path='/' component={GMap} />
+          <Route path="/camera" component={CameraPhoto} />
+          <Route path="/place" component={Place} />
+        </Router>
     // } else {
     //   initialView = 
     //     <div>
