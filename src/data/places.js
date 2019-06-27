d94a61901fc80bea6d69b9e4977e27c2dfe2e67a
Thomas GAUZIN <thomas.gauzin@gmail.com>
2019-06-27 11:09:25 +0200

    end json update

--- src/data/places.js
+++ src/data/places.js
@@ -12,12 +12,12 @@ export default [
     content: {
       introduction: "La place de la Bourse de Bordeaux, à l'origine baptisée place Royale, a été réalisée sous l’intendance de Claude Boucher par l'architecte du roi, Ange-Jacques Gabriel, entre 1730 et 17551.",
       histoire: "La place de la Bourse est la première brèche dans les remparts du Moyen Âge et est destinée à servir de somptueux écrin à la statue équestre, détruite à la Révolution, du roi de France Louis XV. Inaugurée en 1749, elle est le symbole de la prospérité de la ville. Successivement appelée place Royale, place de la Liberté pendant la Révolution, place impériale sous Napoléon Ier, elle retrouve son nom initial à la Restauration. En 1848, à la chute de Louis-Philippe Ier, elle prend le nom de place de la Bourse.",
-      funfact:"La bourse ne fait pas référence au marché financier mais plutôt à la grosse paire de Napoléon 1er, commanditaire de la place"
+      funfact: "Auparavant, la Place de la Bourse a eu plusieurs noms : Place Royale, Place de la Liberté et même Place Impériale."
     },
     pictures: {
       blue: {
-        small: "https://media.routard.com/image/39/7/dsc_6751.1379397.sq150.jpg",
-        big: "http://blogs.furman.edu/frn325fall2015/files/2015/11/aujourd-hui-les-quais-de-bordeaux-et-le-miroir-d-eau-sont_2171584_1200x800.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/place-de-la-bourse.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/place-de-la-bourse-590.jpg"
       },
       present: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/place_bourse/avant.svg",
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/place_bourse/apres.svg"
@@ -40,8 +40,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "https://www.lecercledesliberaux.com/wp-content/uploads/2018/08/Cite-du-Vin-Credit-Arnaud-Bertrande_format_1600x900-150x150.jpg",
-        big: "https://cdn.civitatis.com/francia/burdeos/galeria/museo-vino.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/grand-theatre.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/grand-theatre.jpg"
       },
       old: "https://ecolobordeauxblog.files.wordpress.com/2015/07/la-future-cite-des-civilisations-du-vin-en-construction.jpg"
     }
@@ -50,8 +50,8 @@ export default [
     id: 3,
     name: "ECV digital",
     addresse: "5 place Alice Girou, 33300 Bordeaux",
-    marker: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/tour-bleu.svg",
-    activemarker: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/tour-orange.svg",
+    marker: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/ecv-bleu.svg",
+    activemarker: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/ecv-orange.svg",
     position: {
       lat: 44.8616,
       lng: -0.5549,
@@ -86,8 +86,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "https://www.lecercledesliberaux.com/wp-content/uploads/2018/08/Cite-du-Vin-Credit-Arnaud-Bertrande_format_1600x900-150x150.jpg",
-        big: "https://cdn.civitatis.com/francia/burdeos/galeria/museo-vino.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/quinconces.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/quinconces-590.jpg"
       },
       old: "https://ecolobordeauxblog.files.wordpress.com/2015/07/la-future-cite-des-civilisations-du-vin-en-construction.jpg"
     } 
@@ -103,14 +103,14 @@ export default [
       lng: -0.561097,
     },
     content: {
-      introduction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
-      histoire: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
-      funfact:"Girondiiiiins"
+      introduction: "La gare de Bordeaux-Bastide, anciennement gare de Bastide-Orléans, a été inaugurée le 20 septembre 18521 pour accueillir le chemin de fer Paris-Bordeaux .Le bâtiment, situé quai des Queyries dans le quartier de la Bastide accueille aujourd'hui un complexe de salles de cinéma.",
+      histoire: "La gare Bordeaux-Bastide, édifiée en 1852, est l'une des plus anciennes gares de France. Le 20 septembre 1852, elle accueillit ses premiers trains en direction d'Angoulême. Les premiers trains en provenance de Paris arrivèrent en juillet 18533. En 1859, une halle marchandise est construite, ainsi que la Halle des Magasins Généraux de Bordeaux, premier embranchement privé de la Bastide3. Le 25 août 1860, le pont sur la Garonne surnommé passerelle Eiffel a permis la liaison ferroviaire entre ce réseau et celui de la compagnie du Midi, propriétaire de la ligne de Bordeaux-Saint-Jean à Sète-Ville. Dès 1861, elle se retrouve ainsi amputée d'une partie de son trafic passager au profit de la gare Saint-Jean. Elle accueillera un trafic passagers d'intérêt local jusqu'en 19513 pour se retrouver complètement abandonnée en 1990.",
+      funfact:" Cette halle s'est effondrée en 1950. La statue monumentale de la façade a disparu."
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/orlean.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/orlean-590.jpg"
       },
       present: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg",
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
@@ -133,8 +133,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/base-sous-marine.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/base-sous-marine-590.jpg"
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
@@ -156,8 +156,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/cite-du-vin.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/cite-du-vin-590.jpg"
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
@@ -179,8 +179,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/darwin.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/darwin-590.jpg"
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
@@ -196,14 +196,14 @@ export default [
       lng: -0.571356,
     },
     content: {
-      introduction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
-      histoire: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
-      funfact:"Girondiiiiins"
+      introduction: "La grosse cloche de Bordeaux est le beffroi de l'ancien hôtel de ville. La porte et les tours, dites de la Grosse Cloche, sont classées au titre des monuments historiques par arrêté du 12 juillet 1886.",
+      histoire: "C'est un des rares monuments civils (avec la porte Cailhau) que la ville conserve du Moyen Âge. Elle a été restaurée du 4 au 22 juillet 20162. Elle a été édifiée au xve siècle sur les restes de l'ancienne Porte Saint-Éloy (dite aussi porte Saint James) du xiiie siècle (adossée à l’église Saint-Éloi du xiie siècle), ouverte sur le rempart du xiiie siècle et sous laquelle passaient les pèlerins de Saint-Jacques en route pour Compostelle. D'où le nom de la rue Saint-James voisine, Saint-James étant le nom gascon de Saint-Jacques3. Elle est composée de deux tours circulaires de 40 mètres de haut reliées par un bâtiment central et dominée par le léopard d'or. À l'origine c'était un ensemble de quatre tours rondes et crenelées auxquelles furent adjointes, au xiie siècle, deux autres tours et ne s'élevait que d'un étage. Ces deux dernières se situaient à l'emplacement du milieu de l'actuel cours Victor Hugo qui était à l'époque un fossé longeant le rempart.",
+      funfact:"La cloche actuelle pèse 7 800kg pour deux mètres de hauteur et de diamètre."
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/grosse-cloche.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/grosse-cloche-590.jpg"
       },
       present: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/grosse_cloche/avant.jpg",
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/grosse-cloche/apres.jpg"
@@ -226,8 +226,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/pey-berland.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/pey-berland-590.jpg"
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
@@ -249,8 +249,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/pont-chaband.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/pont-chaband-590.jpg"
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
@@ -272,8 +272,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/pont-de-pierre.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/pont-de-pierre-590.jpg"
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
@@ -295,8 +295,8 @@ export default [
     },
     pictures: {
       current: {
-        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/120/porte-cailhau.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/590/porte-cailhau-590.jpg"
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
