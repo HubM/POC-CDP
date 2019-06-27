63bddf5a7497e72915f57909f662497d3fa90d6b
Thomas GAUZIN <thomas.gauzin@gmail.com>
2019-06-27 15:05:18 +0200

    json

--- src/data/places.js
+++ src/data/places.js
@@ -353,6 +353,30 @@ export default [
       },
       old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
     } 
+  },
+  {
+    id: 15,
+    name: "CIAP Bordeaux Patrimoine",
+    addresse: "4 Place de la Bourse, 33000 Bordeaux",
+    marker: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/CIAP-ORANGE.svg",
+    // activemarker: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/saint-michel-orange.svg",
+    position: {
+      lat: 44.834366,
+      lng: -0.564380,
+    },
+    content: {
+      introduction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
+      histoire: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
+      funfact: [],
+      horaires: ""
+    },
+    pictures: {
+      current: {
+        small: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
+        big: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
+      },
+      old: "http://www.m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
+    } 
   }
 ]
   
