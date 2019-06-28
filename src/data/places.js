8358027b2368fa821c57be1dcd360d27fbd88ca0
HubM <hubmoncenis@gmail.com>
2019-06-28 12:25:31 +0200

    :fire: removing basilique st mich

--- src/data/places.js
+++ src/data/places.js
@@ -328,29 +328,6 @@ export default [
   },
   {
     id: 14,
-    name: "Basilique de Saint-Michel",
-    addresse: "Place Duburg, 33800 Bordeaux",
-    marker: "https://m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/saint-michel-bleu.svg",
-    activemarker: "https://m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/saint-michel-orange.svg",
-    position: {
-      lat: 44.834366,
-      lng: -0.564380,
-    },
-    content: {
-      introduction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
-      histoire: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget risus bibendum, lobortis ante eu, vehicula dui. Nulla rutrum dignissim risus, sit amet dapibus est. Suspendisse ac sapien commodo lacus aliquam vulputate. Donec scelerisque tellus ut libero rutrum rhoncus. Aenean id commodo turpis. Donec mattis odio vitae nunc ornare volutpat. In hac habitasse platea dictumst. In molestie efficitur ligula, sed suscipit lacus mattis ac. Vivamus eget mauris ac diam laoreet efficitur. Aliquam mollis lorem vitae orci consectetur, vitae consectetur leo finibus. Nunc dignissim dapibus interdum. Donec et dolor posuere, bibendum arcu a, vestibulum augue.",
-      funfact: []
-    },
-    pictures: {
-      current: {
-        small: "https://m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg",
-        big: "https://m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/avant.jpg"
-      },
-      old: "https://m2groupe4.ecvdigitalbdx.fr/assets/img/gare_d_orleans/apres.jpg"
-    } 
-  },
-  {
-    id: 15,
     name: "CIAP Bordeaux Patrimoine",
     addresse: "4 Place de la Bourse, 33000 Bordeaux",
     marker: "https://m2groupe4.ecvdigitalbdx.fr/assets/img/pictos-svg/CIAP-ORANGE.svg",
