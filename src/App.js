014aa3d97a542a5612f6fe035a9b84098696628e
HubM <hubmoncenis@gmail.com>
2019-06-28 09:04:04 +0200

    :art: redirect https if prod and http protocol

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Routeur from './Routeur';
 import './styles/index.scss';
 
 export class App extends React.Component {
+  componentWillMount() {
+    if (process.env.NODE_ENV === "production" && window.location.protocol === "http:") {
+      window.location.replace('https://cdp2021.herokuapp.com/#');
+    }
+  }
   render() {
     return (
       <Routeur />
